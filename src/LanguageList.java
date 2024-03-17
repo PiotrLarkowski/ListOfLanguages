0d6310d582ca4333b97b47f4eb6b5a2dc6e44c53
Piotr <P.Larkowski90@gmail.com>
2024-03-17 14:11:55 +0100

    First method implemented isEmpty()

---
+++ src/LanguageList.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LanguageList {
+    private final List<String> languages = new ArrayList<>();
+
+    public boolean isEmpty() {
+        return languages.isEmpty();
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        LanguageList languageList = new LanguageList();
+        System.out.println("Is My list of languages empty? ");
+        System.out.println(languageList.isEmpty() + "\n");
+
+    }
+}
