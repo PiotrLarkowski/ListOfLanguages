adf8aeab13b05641c72bf7f22f77ed117d0b8ec3
Piotr <P.Larkowski90@gmail.com>
2024-03-17 14:22:15 +0100

    Add second option to add a new language to list

--- src/LanguageList.java
+++ src/LanguageList.java
@@ -4,8 +4,14 @@ import java.util.List;
 public class LanguageList {
     private final List<String> languages = new ArrayList<>();
 
+    public int sizeOfList(){
+        return languages.size();
+    }
+
     public boolean isEmpty() {
         return languages.isEmpty();
     }
-
+    public void addLanguage(String language) {
+        languages.add(language);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         LanguageList languageList = new LanguageList();
-        System.out.println("Is My list of languages empty? ");
-        System.out.println(languageList.isEmpty() + "\n");
+        System.out.print("Is My list of languages empty? ");
+        System.out.println(languageList.isEmpty());
+        System.out.println("I want to add language English to my list.");
+        System.out.println("Language size before added new language: " + languageList.sizeOfList());
+        languageList.addLanguage("English");
+        System.out.println("Language size after added new language: " + languageList.sizeOfList());
 
     }
 }
